6e4bd4140da63c08c9f084165ed578464c621202
Grigory <rehor.ger@gmail.com>
2023-02-24 18:04:09 +0100

    404 page styles refactoring done, component styles minor refactoring done, 404 rerouting to main page implemented

--- src/shared/assets/ui/icons/icons.sidebar/CollapseIcon.tsx
+++ src/shared/assets/ui/icons/icons.sidebar/CollapseIcon.tsx
@@ -1,8 +1,11 @@
+import { type IIconProps } from "../../interfaces/IIconProps";
 import classes from "../../styles/Icon.module.scss";
+import { getClassNames } from "shared/lib";
+import { type FC } from "react";
 
-export const CollapseIcon = (): JSX.Element | null => {
+export const CollapseIcon: FC<IIconProps> = ({ classNames }): JSX.Element | null => {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={classes.svgicon}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={getClassNames(classes.svgicon, {}, classNames)}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5" />
         </svg>
     );
